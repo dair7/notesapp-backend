1314a17e88b26ba6ee52c64b8f1b893f5569b7a0
Dair gonzalez <dairplaza77@gmail.com>
2026-03-13 19:35:57 -0500

    add entity usuario

---
+++ notesapp/src/main/java/com/notesapp/entity/Usuario.java
@@ -0,0 +1,39 @@
+package com.notesapp.entity;
+
+import com.notesapp.enums.RoleType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String nombre;
+
+    @Column(nullable = false, unique = true, length = 150)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 30)
+    private RoleType role = RoleType.USER;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+}

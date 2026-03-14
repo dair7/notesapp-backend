2169ace99f532fa1efe50c97953dddaf9b096236
Dair gonzalez <dairplaza77@gmail.com>
2026-03-13 19:33:42 -0500

    add entity nota

---
+++ notesapp/src/main/java/com/notesapp/entity/Nota.java
@@ -0,0 +1,49 @@
+package com.notesapp.entity;
+
+import com.notesapp.enums.EstadoNoteType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "notas")
+public class Nota {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 200)
+    private String titulo;
+
+    @Column(columnDefinition = "TEXT")
+    private String contenido;
+
+    @CreationTimestamp
+    @Column(name = "fecha_creacion", nullable = false, updatable = false)
+    private LocalDateTime fechaCreacion;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, columnDefinition = "varchar default 'ACTIVA'")
+    private EstadoNoteType estado = EstadoNoteType.ACTIVA;
+
+    @Column(name = "es_anclada", nullable = false, columnDefinition = "boolean default false")
+    private boolean esAnclada = false;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "usuario_id",  nullable = false)
+    private Usuario usuario;
+
+    // Al eliminar una nota, se eliminan sus recordatorios automáticamente
+    @OneToMany(mappedBy = "nota", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Recordatorio> recordatorios;
+
+}

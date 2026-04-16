9443b229eee857324cda140999665ed82585cd95
Dair gonzalez <dairplaza77@gmail.com>
2026-04-15 21:03:36 -0500

    remove @Future validation due to timezone conflict with Flutter client

--- notesapp/src/main/java/com/notesapp/dto/recordatorioDTO/RecordatorioRequestDTO.java
+++ notesapp/src/main/java/com/notesapp/dto/recordatorioDTO/RecordatorioRequestDTO.java
@@ -1,7 +1,6 @@
 package com.notesapp.dto.recordatorioDTO;
 
 import com.notesapp.enums.Prioridad;
-import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -15,7 +14,6 @@ import java.time.LocalDateTime;
 public class RecordatorioRequestDTO {
 
     @NotNull(message = "La fecha de la tarea es obligatoria")
-    @Future(message = "La fecha de la tarea debe ser en el futuro")
     private LocalDateTime fecha;
 
     private Prioridad prioridad = Prioridad.MEDIA;
